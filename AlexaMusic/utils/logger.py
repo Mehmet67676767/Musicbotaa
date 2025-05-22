298865ba51f02eb241b8f2638d5a2a0ff93dc8ae
Mehmet67676767 <kurt67143@gmail.com>
2025-05-22 20:47:21 +0300

    Update logger.py

--- AlexaMusic/utils/logger.py
+++ AlexaMusic/utils/logger.py
@@ -1,44 +1,34 @@
-# Copyright (C) 2025 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
 
-"""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2021 ~ Present Team Alexa <https://github.com/TheTeamAlexa>
+import logging
+from colorama import init, Fore, Style
 
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
+init(autoreset=True)
 
-from pyrogram.enums import ParseMode
+LEVEL_COLORS = {
+    "DEBUG": Fore.BLUE,
+    "INFO": Fore.GREEN,
+    "WARNING": Fore.YELLOW,
+    "ERROR": Fore.RED,
+    "CRITICAL": Fore.RED + Style.BRIGHT,
+}
 
-from config import LOG_GROUP_ID
-from AlexaMusic.utils.database import is_on_off
-from AlexaMusic import app
+class ColoredFormatter(logging.Formatter):
+    def format(self, record):
+        levelname_color = LEVEL_COLORS.get(record.levelname, "")
+        record.levelname = f"{levelname_color}{record.levelname}{Style.RESET_ALL}"
+        record.name = f"{Fore.CYAN}{record.name}{Style.RESET_ALL}"
+        record.msg = f"{Fore.WHITE}{record.msg}{Style.RESET_ALL}"
+        return super().format(record)
 
+def LOGGER(name: str):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
 
-async def play_logs(message, streamtype):
-    if await is_on_off(2):
-        logger_text = f"""
-<b>{app.mention} 𝖯𝗅𝖺𝗒 𝖫𝗈𝗀</b>
+    handler = logging.StreamHandler()
+    formatter = ColoredFormatter("[%(asctime)s] %(name)s - %(levelname)s - %(message)s", "%H:%M:%S")
+    handler.setFormatter(formatter)
 
-<b>𝖢𝗁𝖺𝗍 𝖨𝖣 :</b> <code>{message.chat.id}</code>
-<b>𝖢𝗁𝖺𝗍 𝖭𝖺𝗆𝖾 :</b> {message.chat.title}
-<b>𝖢𝗁𝖺𝗍 𝖴𝗌𝖾𝗋𝗇𝖺𝗆𝖾 :</b> @{message.chat.username}
+    if not logger.hasHandlers():
+        logger.addHandler(handler)
 
-<b>𝖴𝗌𝖾𝗋 𝖨𝖣 :</b> <code>{message.from_user.id}</code>
-<b>𝖴𝗌𝖾𝗋 𝖭𝖺𝗆𝖾 :</b> {message.from_user.mention}
-<b>𝖴𝗌𝖾𝗋𝗇𝖺𝗆𝖾 :</b> @{message.from_user.username}
-
-<b>𝖰𝗎𝖾𝗋𝗒 :</b> {message.text.split(None, 1)[1]}
-<b>𝖲𝗍𝗋𝖾𝖺𝗆-𝖳𝗒𝗉𝖾 :</b> {streamtype}"""
-        if message.chat.id != LOG_GROUP_ID:
-            try:
-                await app.send_message(
-                    chat_id=LOG_GROUP_ID,
-                    text=logger_text,
-                    parse_mode=ParseMode.HTML,
-                    disable_web_page_preview=True,
-                )
-            except Exception:
-                pass
-        return
+    return logger
