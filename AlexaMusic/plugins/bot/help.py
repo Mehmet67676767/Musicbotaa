8cb0815f4565b962757dfa338075e16f10fd8a58
Mehmet67676767 <kurt67143@gmail.com>
2025-05-25 20:35:11 +0300

    Update help.py

--- AlexaMusic/plugins/bot/help.py
+++ AlexaMusic/plugins/bot/help.py
@@ -77,7 +77,7 @@ async def helper_cb(client, CallbackQuery, _):
     if cb == "hb5":
         if CallbackQuery.from_user.id not in SUDOERS:
             return await CallbackQuery.answer(
-                "ᴏɴʟʏ ғᴏʀ ᴏᴡɴᴇʀ ᴀɴᴅ sᴜᴅᴏᴇʀs", show_alert=True
+                "ᴏɴʟʏ ғᴏʀ ᴏᴡɴᴇʀ ᴀɴᴅ sᴜᴅᴏᴇʀs", show_alert=False
             )
         await CallbackQuery.edit_message_text(helpers.HELP_5, reply_markup=keyboard)
         return await CallbackQuery.answer()
